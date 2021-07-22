6fd8d1f4d437afaadb81b5b1882bcfaa9a9b9464
taoxiantong <taoxiantong@qq.com>
2021-07-22 21:42:42 +0800

    update 5.boost

--- Chapter2/code/5.cpp
+++ Chapter2/code/5.cpp
@@ -1,8 +1,7 @@
-# include "boost/scoped_ptr.hpp"
 # include "iostream"
 using namespace std;
 
-int main()
+void scope_test()
 {
     boost::scoped_ptr<int> i(new int);
     cout<<"i: "<< i <<endl;    
@@ -15,5 +14,23 @@ int main()
     
     *i = 3;
     cout<<"i: "<< i <<endl; 
+
+}
+
+void array_test()
+{
+    boost::scoped_array<int> i(new int[2]);
+    cout<<"i: "<<i <<endl;
+    *i.get() = 1;
+    cout<<"*i.get() : "<< i <<endl;
+    i[1] = 2;
+    cout<<"i[1]: " << i[0] << " , " << i[1] << endl;
+    i.reset(new int[3]);
+
+}
+
+int main()
+{
+    array_test();
     return 0;
 }
