e125a5e8191732c0c274a593cc326c63ebfec298
taoxiantong <taoxiantong@qq.com>
2021-07-24 19:59:21 +0800

    update to code sample

--- Chapter1/code/26.Multithreading.cpp
+++ Chapter1/code/26.Multithreading.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <thread>
 
-void C11thread()
+void thread_1()
 {
-    std::thread t1(t1recall);
-    t1.join();
-
+    std::cout<<"thread_1 excuting"<<std::endl;
 }
 
 int main()
 {
+    std::thread threadObj1(thread_1);
+    std::thread threadObj2(thread_1);
+    
+    if(threadObj1.get_id() != threadObj2.get_id())
+    {
+        std::cout<<"Both have diffent ID."<<std::endl;
+    }
+    
+        
+    std::cout<<"threadObj1 ID is: "<< threadObj1.get_id() <<std::endl;
+    std::cout<<"threadObj2 ID is: "<< threadObj2.get_id() <<std::endl;
+
+    threadObj1.join();
+    threadObj2.join();
+    
+    
+    std::cout<<"Exit of main function"<<std::endl;
     return 0;
 }
